e0808518aa8ddb616ed68283439538fa8acb7971
itisalbin <to.albin@gmail.com>
2021-12-19 12:20:25 +0100

    Layout on log-in page done. Added head include.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './resources/**/*.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'raleway-light': ['Raleway'],
+      },
+    },
   },
   plugins: [],
 };
