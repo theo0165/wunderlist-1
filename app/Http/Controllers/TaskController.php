2f36d0bd4deea45a863122bd032861d5fbafd7ef
itisalbin <to.albin@gmail.com>
2021-12-25 20:55:32 +0100

    Added info text when no tasks are available.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -16,7 +16,6 @@ class TaskController extends Controller
 {
     public function request(Request $request)
     {
-
         if ($request->has('task_id')) { //Load the form with task_id
             $taskid = $request->input('task_id');
             $task = Task::where('user_id', auth()->id())->where('id', $taskid)->get()->toArray();
@@ -24,40 +23,39 @@ class TaskController extends Controller
             return view("edittask", ['task' => $task[0]]);
         }
 
-        if ($request->input('request') === 'store') { //Store
-            $task = new Task;
-            $task->user_id = auth()->id();
-            $task->title = $request->input('title');
-            $task->description = $request->input('description');
-            $task->deadline = $request->input('deadline');
-            $task->list_id = $request->input('listid');
-            if ($request->has('completed')) {
-                $task->completed = true;
-            } else {
-                $task->completed = false;
-            }
-            $task->save();
-            return redirect()->route("tasks");
-        } else if ($request->input('request') === 'update') { //Update
-
-            $isCompleted = false;
-            if ($request->has('completed')) {
-                $isCompleted = true;
-            }
-
-            Task::where('id', $request->input('id')) //Is this safe, could you change the id in the browser and change other's tasks?
-                ->update([
-                    'title' => $request->input('title'),
-                    'description' => $request->input('description'),
-                    'deadline' => $request->input('deadline'),
-                    'list_id' => $request->input('list_id'),
-                    'completed' => $isCompleted
-                ]);
-        } else { //Delete
-            Task::where('id', $request->input('id'))->delete();
+        switch ($request->input('request')) {
+            case 'store':
+                $task = new Task;
+                $task->user_id = auth()->id();
+                $task->title = $request->input('title');
+                $task->description = $request->input('description');
+                $task->deadline = $request->input('deadline');
+                $task->list_id = $request->input('listid');
+                if ($request->has('completed')) {
+                    $task->completed = true;
+                } else {
+                    $task->completed = false;
+                }
+                $task->save();
+                return redirect()->route("tasks");
+            case 'update':
+                $isCompleted = false;
+                if ($request->has('completed')) {
+                    $isCompleted = true;
+                }
+                Task::where('id', $request->input('id')) //Is this safe, could you change the id in the browser and change other's tasks?
+                    ->update([
+                        'title' => $request->input('title'),
+                        'description' => $request->input('description'),
+                        'deadline' => $request->input('deadline'),
+                        'list_id' => $request->input('list_id'),
+                        'completed' => $isCompleted
+                    ]);
+                return redirect()->to($request->input('back_url'));
+            case 'delete':
+                Task::where('id', $request->input('id'))->delete();
+                return redirect()->to($request->input('back_url'));
         }
-
-        return redirect()->to($request->input('back_url'));
     }
 
     public function load()
@@ -70,7 +68,6 @@ class TaskController extends Controller
     public function loadToday()
     {
         $tasks = Task::where('user_id', auth()->id())->get()->toArray();
-
         $tasks = array_filter($tasks, function ($task) {
             $today = new DateTime('today'); //Timezone?
             $today = $today->getTimestamp();
