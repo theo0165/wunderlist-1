c0c24f33d0a21dbd54092426a77f0ed2e1730fec
itisalbin <to.albin@gmail.com>
2022-01-01 19:06:47 +0100

    Changed icons and styled hamburger menu.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -11,18 +11,26 @@
     <div>
         <nav class="max-w-[720px] h-12 m-auto flex relative">
             <div class="w-1/3 h-full"></div>
-            <div class="w-1/3 h-full"><img src="images/logo.svg" alt="" class="h-full m-auto"></div>
-            <div class="w-1/3 h-full flex justify-end items-center">
-                <div class="w-9 h-8 mr-2 flex flex-col justify-between" id="hamburger">
+            <div class="w-1/3 h-full relative"><img src="images/logo.svg" alt="" class="h-full m-auto z-10 absolute left-1/2 -translate-x-1/2"></div>
+            <div class="w-1/3 h-full flex justify-end items-center relative z-10">
+                <div class="w-7 h-6 mr-2 flex flex-col justify-between absolute" id="hamburger">
                     <div class="w-full h-1/6 bg-mainblue-600 rounded-full"></div>
                     <div class="w-full h-1/6 bg-mainblue-600 rounded-full"></div>
                     <div class="w-full h-1/6 bg-mainblue-600 rounded-full"></div>
                 </div>
             </div>
+            <div class="w-full m-auto right-0 h-screen bg-mainblue-600 absolute z-2 flex justify-center items-center" id="hamburger-panel">
+                <div class="flex flex-col gap-6">
+                    <a href="" class="flex gap-1"><img src="images/gear-icon.svg" alt="" class="w-6 h-6">
+                        <p class="text-white">Settings</p>
+                    </a>
+                    <a href="" class="flex gap-1"><img src="images/logout-icon.svg" alt="" class="w-6 h-6">
+                        <p class="text-white">Log out</p>
+                    </a>
+                </div>
+            </div>
         </nav>
 
-        <div class="w-full right-0 h-[calc(100vh-3rem)] bg-mainblue-600 absolute" id="hamburger-panel">
-        </div>
 
         <main class="max-w-sm m-auto">
             <div class="w-full flex flex-col items-center mt-12">
@@ -46,10 +54,26 @@
 
         <script>
             const hamburger = document.getElementById("hamburger");
+            const bars = hamburger.querySelectorAll("div");
             const hamburgerPanel = document.getElementById("hamburger-panel");
+            const logo = document.querySelector("nav img");
+            let isOpen = false;
             hamburgerPanel.classList.add('hidden');
             hamburger.addEventListener('click', () => {
                 hamburgerPanel.classList.toggle('hidden');
+                if (!isOpen) {
+                    logo.src = 'images/logo-white.svg';
+                    bars.forEach((bar) => {
+                        bar.style.backgroundColor = "white";
+                    });
+                    isOpen = true;
+                } else {
+                    logo.src = 'images/logo.svg';
+                    bars.forEach((bar) => {
+                        bar.style.backgroundColor = "#0061BB";
+                    });
+                    isOpen = false;
+                }
             });
         </script>
 
